71c715547e9e0a2ba64ce5a69b3fbc36e73d78fb
Kyle Stanfield <kjstan@gmail.com>
2024-03-10 20:07:38 -0400

    Update app.js to V2

--- app.js
+++ app.js
@@ -1,31 +1,72 @@
-const team1 = [
-  { type: "3", id: "4611686018482605597" },
-  { type: "1", id: "4611686018456289666" },
-  { type: "3", id: "4611686018472105534" },
-  { type: "3", id: "4611686018468117711" },
-  { type: "3", id: "4611686018504800722" },
-  { type: "1", id: "4611686018430872228" },
-];
-
-const team2 = [
-  { type: "3", id: "4611686018467847265" },
-  { type: "1", id: "4611686018434321149" },
-  { type: "1", id: "4611686018429882221" },
-  { type: "1", id: "4611686018432209461" },
-  { type: "1", id: "4611686018429879860" },
-  { type: "1", id: "4611686018432622059" },
-];
-
 let url = "https://www.bungie.net/Platform/Destiny2/";
 
-//`${url}${p.type}/Profile/${p.id}?components=1100"`
+const allTeams = [
+  [
+    { type: "3", id: "4611686018482605597", name: "Butters" },
+    { type: "1", id: "4611686018456289666", name: "Frzn" },
+    { type: "3", id: "4611686018472105534", name: "Razorcut" },
+    { type: "3", id: "4611686018468117711", name: "Ksetcs" },
+    { type: "3", id: "4611686018504800722", name: "Apple_64" },
+    { type: "1", id: "4611686018430872228", name: "Reopss" },
+  ],
+  [
+    { type: "3", id: "4611686018467847265", name: "AlexK" },
+    { type: "1", id: "4611686018434321149", name: "Action Jackson" },
+    { type: "1", id: "4611686018429882221", name: "Gally" },
+    { type: "1", id: "4611686018432209461", name: "ThwartGolf" },
+    { type: "1", id: "4611686018429879860", name: "AsianSpecialist" },
+    { type: "1", id: "4611686018432622059", name: "Grizzly" },
+  ],
+
+  [
+    { type: "1", id: "4611686018429860638", name: "Sherpa Mind" },
+    { type: "1", id: "4611686018429810474", name: "BonaFideHiro" },
+    { type: "1", id: "4611686018430335839", name: "Revenant" },
+    { type: "1", id: "4611686018429826794", name: "Fletcher117" },
+    { type: "1", id: "4611686018433084550", name: "Mm2mmrimmer" },
+    { type: "1", id: "4611686018429541409", name: "Jarv" },
+  ],
+
+  [
+    { type: "1", id: "4611686018473082405", name: "Miss" },
+    { type: "2", id: "4611686018473387231", name: "Fangz" },
+    { type: "1", id: "4611686018449648082", name: "Haxx" },
+    { type: "1", id: "4611686018451363932", name: "ShieldMaiden" },
+    { type: "3", id: "4611686018484174236", name: "TechStomper" },
+  ],
+
+  [
+    { type: "2", id: "4611686018512016613", name: "Mytho" },
+    { type: "1", id: "4611686018433696774", name: "Shadowstep" },
+    { type: "3", id: "4611686018504265789", name: "Blue" },
+    { type: "3", id: "4611686018530253396", name: "JUICER" },
+    { type: "1", id: "4611686018461669902", name: "Cryo" },
+    { type: "1", id: "4611686018464050897", name: "Caracals" },
+  ],
+
+  [
+    { type: "1", id: "4611686018430246885", name: "Qanzmoto" },
+    { type: "3", id: "4611686018496005145", name: "HarDJunk" },
+    { type: "2", id: "4611686018443998952", name: "Lover" },
+    { type: "1", id: "4611686018445376126", name: "Instinct" },
+    { type: "3", id: "4611686018495414673", name: "Kalista" },
+    { type: "2", id: "4611686018458773501", name: "Cirisly" },
+  ],
+];
 
 function run() {
   let input = document.getElementById("key");
-  let team1score = 0;
-  let team2score = 0;
-  document.getElementById("t1").innerHTML = "loading...";
-  document.getElementById("t2").innerHTML = "loading...";
+
+  if (input.value == null || input.value == "") {
+    console.log("No Key");
+    return;
+  }
+
+  input.classList.add("hidden");
+  let loadedScores = document.getElementsByClassName("score");
+  for (i = 0; i < loadedScores.length; i++) {
+    loadedScores[i].innerHTML = "loading...";
+  }
 
   function getData(object) {
     const apiUrl = `${url}${object.type}/Profile/${object.id}?components=1100`;
@@ -39,44 +80,37 @@ function run() {
 
     return fetch(apiUrl, requestOptions).then((response) => {
       if (!response.ok) {
-        throw new Error(`Failed to fetch data for object with ID ${object.id}`);
+        throw new Error(`Failed to fetch data for ${object.name}`);
       }
       return response.json();
     });
   }
 
-  //team 1 *****************
-  Promise.all(team1.map((object) => getData(object)))
-    .then((results) => {
-      results.forEach(
-        (score) =>
-          (team1score =
-            team1score +
-            score.Response.metrics.data.metrics[2330926603].objectiveProgress
-              .progress)
-      );
+  function getScores(team, count) {
+    let score = 0;
+    let currentTeam = `team${count}`;
+    let scoreDisplay = document.getElementById(currentTeam);
 
-      console.log("Team 1 Score:", team1score);
-      document.getElementById("t1").innerHTML = team1score;
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
+    Promise.all(team.map((object) => getData(object)))
+      .then((results) => {
+        console.log(`
+        *------ ${currentTeam} ------*
+        `);
+        results.forEach((player, i) => {
+          let curScore =
+            player.Response.metrics.data.metrics[2330926603].objectiveProgress
+              .progress;
+          score = score + curScore;
+          console.log(`${team[i].name} -> ${curScore}`);
+        });
+        scoreDisplay.innerHTML = score;
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }
 
-  //team 2 *****************
-  Promise.all(team2.map((object) => getData(object)))
-    .then((results) => {
-      results.forEach(
-        (score) =>
-          (team2score =
-            team2score +
-            score.Response.metrics.data.metrics[2330926603].objectiveProgress
-              .progress)
-      );
-      console.log("Team 2 Score:", team2score);
-      document.getElementById("t2").innerHTML = team2score;
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
+  allTeams.forEach((team, i) => {
+    getScores(team, `${i + 1}`);
+  });
 }
