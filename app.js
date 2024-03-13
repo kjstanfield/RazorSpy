bdded713db6b1bfad1ee25f6e5c67eb560d28713
Kyle Stanfield <kjstan@gmail.com>
2024-03-13 15:04:15 -0400

    Testing Sorting

--- app.js
+++ app.js
@@ -58,6 +58,30 @@ const allTeams = [
   ],
 ];
 
+function sortTable() {
+  var table, rows, switching, i, x, y, shouldSwitch;
+  table = document.getElementById("scoreTable");
+  switching = true;
+
+  while (switching) {
+    switching = false;
+    rows = table.rows;
+    for (i = 1; i < rows.length - 1; i++) {
+      shouldSwitch = false;
+      x = rows[i].getElementsByTagName("TD")[1];
+      y = rows[i + 1].getElementsByTagName("TD")[1];
+      if (parseInt(x.innerHTML) > parseInt(y.innerHTML)) {
+        shouldSwitch = true;
+        break;
+      }
+    }
+    if (shouldSwitch) {
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+    }
+  }
+}
+
 function run() {
   let input = document.getElementById("key");
   console.log(`NEW RUN`);
@@ -108,6 +132,7 @@ function run() {
           console.log(`${team[i].name} -> ${curScore}`);
         });
         scoreDisplay.innerHTML = score;
+        sortTable();
       })
       .catch((error) => {
         console.error("Error:", error);
